d78abd9925925ccf23971e185704385bb7e42b87
stebomurkn420 <stevn4127@gmail.com>
2019-06-29 05:39:25 -0400

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -26,7 +26,7 @@ TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
 # Include DU common configuration
-include vendor/bootleggers/config/common_full_phone.mk
+include vendor/liquid/config/common_full_phone.mk
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/google/bonito/aosp_bonito.mk)
@@ -35,7 +35,7 @@ $(call inherit-product, device/google/bonito/aosp_bonito.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-PRODUCT_NAME := bootleg_bonito
+PRODUCT_NAME := liquid_bonito
 PRODUCT_DEVICE := bonito
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 3a XL
@@ -46,6 +46,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS=stebomurkn420 \
     BUILD_FINGERPRINT=google/bonito/bonito:9/PQ3B.190605.006/5560048:user/release-keys \
     PRIVATE_BUILD_DESC="bonito-user 9 PQ3B.190605.006 5560048 release-keys"
-    DEVICE_MAINTAINERS=stebomurkn420 \
 
 $(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)
