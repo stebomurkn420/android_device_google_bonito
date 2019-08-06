c035a245504ccdaf84d4462541353e1d81b48c00
stebomurkn420 <stevn4127@gmail.com>
2019-08-06 01:48:53 -0400

    Update build fingerprint to PQ3B.190801.002

--- liquid.mk
+++ liquid.mk
@@ -44,7 +44,7 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
     DEVICE_MAINTAINERS=stebomurkn420 \
-    BUILD_FINGERPRINT=google/bonito/bonito:9/PQ3B.190705.003/5622519:user/release-keys \
-    PRIVATE_BUILD_DESC="bonito-user 9 PQ3B.190705.003 5622519 release-keys"
+    BUILD_FINGERPRINT=google/bonito/bonito:9/PQ3B.190801.002/5674421:user/release-keys \
+    PRIVATE_BUILD_DESC="bonito-user 9 PQ3B.190801.002 5674421 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)
